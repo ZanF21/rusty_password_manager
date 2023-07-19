dfa674c65edbdd18da2a2fea2cc45318c4990c12
rizan21 <rizanfarooqui@gmail.com>
2023-07-19 16:02:33 +0530

    Added options for type of key - key gen or use old key or paste key

--- src/main.rs
+++ src/main.rs
@@ -33,44 +33,63 @@ fn main() {
 }
 
 fn init() {
-    // TODO: Implement init
-    // 1. Create a directory in the user's home directory called .rusty-pass-manager
-    // 2. passwords are stored in a file with the associated website/app name
-    // 3. The file is encrypted with an ssh key
-    // 4. the directory is also git initialized
-    // if dir already exists, then ask twice if they want to overwrite and also ask for the ssh key
-    // if the dir does not exist, then ask for the ssh key pub and private key and add them to .pub_key and .priv_key files
-    // also create a .gitignore file and add the .priv_key and .pub_key and .gitignore files to it
-
     let dir = format!("{}/.rusty-pass-manager", home_dir().unwrap().display());
+
+    // Check if directory exists
+    if fs::metadata(&dir).is_ok() {
+        println!("Directory already exists: {}", dir);
+        println!("Do you want to overwrite it? (y/N)");
+        let mut overwrite = String::new();
+        std::io::stdin().read_line(&mut overwrite).unwrap();
+        let overwrite = overwrite.trim();
+        if overwrite == "y" {
+            fs::remove_dir_all(&dir).unwrap();
+        } else {
+            println!("Exiting...");
+            return;
+        }
+    }
     fs::create_dir_all(&dir).unwrap();
     let gitignore = format!("{}/.gitignore", dir.clone());
     fs::write(gitignore, ".priv_key\n.pub_key\n.gitignore\n").unwrap();
 
-    print!("{}[2J", 27 as char);
-    // ask for pub and private key
-    println!("Enter your public key:");
-    let mut pub_key = String::new();
-    std::io::stdin().read_line(&mut pub_key).unwrap();
-    let pub_key = pub_key.trim();
+    // Setup Encryption Keys
+    loop {
+        print!("{}[2J", 27 as char);
+        println!("Setting up Encryption Keys");
+        println!("--------------------------");
+        println!("1. Generate new ssh key (not implemented)");
+        println!("2. Use existing ssh key");
+        println!("3. Enter ssh key manually");
 
-    // clear the terminal
-    print!("{}[2J", 27 as char);
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).unwrap();
+        let choice: u8 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid choice");
+                continue;
+            }
+        };
+        match choice {
+            1 => {
+                println!("1 but not implemented");
+                break;
+            }
+            2 => {
+                println!("2 but not implimented");
+                break;
+            }
+            3 => {
+                println!("3 but not implimented");
+                break;
+            }
+            _ => {
+                println!("Invalid choice");
+            }
+        }
+    }
 
-    println!("Enter your private key:");
-    let mut priv_key = String::new();
-    std::io::stdin().read_line(&mut priv_key).unwrap();
-    let priv_key = priv_key.trim();
-
-    print!("{}[2J", 27 as char);
-
-    let pub_key_file = format!("{}/.pub_key", dir.clone());
-    fs::write(pub_key_file, pub_key).unwrap();
-
-    let priv_key_file = format!("{}/.priv_key", dir.clone());
-    fs::write(priv_key_file, priv_key).unwrap();
-
-    // git init
     let git_init = format!("cd {} && git init", dir.clone());
     std::process::Command::new("sh")
         .arg("-c")
@@ -78,9 +97,7 @@ fn init() {
         .output()
         .expect("failed to execute process");
 
-    // git add .gitignore
     let git_add = format!("cd {} && git add *", dir.clone());
-
     std::process::Command::new("sh")
         .arg("-c")
         .arg(git_add)
@@ -90,12 +107,12 @@ fn init() {
     // git commit -m "init"
     println!("starting commit");
     let git_commit = format!("cd {} && git commit -m \"init\"", dir.clone());
-
     std::process::Command::new("sh")
         .arg("-c")
         .arg(git_commit)
         .output()
         .expect("failed to execute process");
 
-    println!("init");
+    println!("created directory: {}", dir);
+    println!();
 }
