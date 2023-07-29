1e615f51183050b8fcf8ae1e50bb2a8f8220eb0c
rizan21 <rizanfarooqui@gmail.com>
2023-07-29 17:29:00 +0530

    trying to figure out how to run global flags without th requirement of subcommands - might just make tree view a subcommand if things dont work out ... i mean it does make sense to do this

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -10,9 +10,11 @@ use subcommands::{add, init, copy, Subcommands};
     long_about = "Don't got time for this sadly .... move along ... come here next time :)"
 )]
 struct Rusty {
-    /// Sets the level of verbosity
-    #[clap(short, long)]
-    pub verbose: bool,
+    /// View all stored Passwords
+    #[arg(long, action = clap::ArgAction::SetTrue, default_value = "false")]
+    pub view: bool,
+    
+    
     #[clap(subcommand)]
     pub subcmd: subcommands::Subcommands,
 }
@@ -20,6 +22,17 @@ struct Rusty {
 pub fn run() {
     let application = Rusty::parse();
 
+    if application.view {
+        // cd to ~/.rusty_password_manager and then tree . -d
+        println!("Viewing all stored Passwords");
+        std::process::Command::new("tree")
+            .arg(".")
+            .arg("-d")
+            .current_dir(std::env::var("HOME").unwrap() + "/.rusty_password_manager")
+            .spawn()
+            .expect("Failed to execute command");
+        return;
+    }
     match application.subcmd {
         Subcommands::Init => {
             println!("Initializing Password Manager");
@@ -36,5 +49,6 @@ pub fn run() {
         } => {
             copy::copy(service_name);
         }
+        
     }
 }
--- src/app/subcommands/mod.rs
+++ src/app/subcommands/mod.rs
@@ -6,9 +6,11 @@ pub mod copy;
 #[derive(Parser, Debug)]
 pub enum Subcommands {
     /// Perform initialization tasks for Password Manager to run
+    #[clap(name = "init")]
     Init,
 
     /// Add a new password (just the password and the name of the service the password is for)
+    #[clap(name = "add")]
     Add {
         /// Name of the service the password is for
         service_name: String,
@@ -16,8 +18,11 @@ pub enum Subcommands {
         password: String,
     },
     /// Copies the password for a given service to the clipboard
+    #[clap(name = "copy")]
     Copy {
         /// Name of the service that you want the password for
         service_name: String,
     },
+
+
 }
